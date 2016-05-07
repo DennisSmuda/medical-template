b4f03c44c64bdff67eac31c4e4cb8a3df51f6403
Dennis Smuda <dennissmuda@msn.com>
2016-05-07 12:16:57 +0200

    Team page added

--- src/SiteBundle/Controller/DefaultController.php
+++ src/SiteBundle/Controller/DefaultController.php
@@ -10,4 +10,8 @@ class DefaultController extends Controller
     {
         return $this->render('SiteBundle:Default:index.html.twig');
     }
+
+    public function teamAction() {
+        return $this->render('SiteBundle:Default:team.html.twig');
+    }
 }
